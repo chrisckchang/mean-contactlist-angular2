ac7b6c7acbca067f0d361259b6b369a70a020b81
Tim Fogarty <tim@tfogo.com>
2018-07-10 13:52:36 -0700

    Remove semicolon

--- src/main.ts
+++ src/main.ts
@@ -9,5 +9,5 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
