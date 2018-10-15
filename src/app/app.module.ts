0e0e33bc20bd4f9de80c49919deec900ebc4963d
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2018-10-15 15:39:51 -0300

    Exibe informações pessoais na página de novo post

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { HttpModule } from "@angular/http";
 
-import { AppComponent } from './app.component';
-import { PostDetailsComponent } from './post/post-details/post-details.component';
-import { PostListComponent } from './post/post-list/post-list.component';
+import { AppComponent } from "./app.component";
+import { PostDetailsComponent } from "./post/post-details/post-details.component";
+import { PostListComponent } from "./post/post-list/post-list.component";
+
+import { AngularFireModule } from "@angular/fire";
+import { AngularFirestoreModule } from "@angular/fire/firestore";
+import { AngularFireAuthModule } from "@angular/fire/auth";
+
+import { firebase } from "../environments/firebase";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    PostDetailsComponent,
-    PostListComponent
-  ],
+  declarations: [AppComponent, PostDetailsComponent, PostListComponent],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/post/post-details/post-details.component.ts
+++ src/app/post/post-details/post-details.component.ts
@@ -1,37 +1,44 @@
-import { Component, Input } from '@angular/core';
-import { Post } from '../post';
-import { PostService } from '../post.service';
+import { Component, Input } from "@angular/core";
+import { Post } from "../post";
+import { PostService } from "../post.service";
+
+import { AuthService } from "../../services/auth.service";
 
 @Component({
-  selector: 'post-details',
-  templateUrl: './post-details.component.html',
-  styleUrls: ['./post-details.component.css']
+  selector: "post-details",
+  templateUrl: "./post-details.component.html",
+  styleUrls: ["./post-details.component.css"]
 })
-
 export class PostDetailsComponent {
-
   eduProgramOptions = [
-    {name:'Criação do ProUni', checked:false},
-    {name:'Criação do ENEM e SiSu', checked:false},
-    {name:'Aumento de vagas nas Universidades Federais', checked:false},
-    {name:'Novas Universidades Federais', checked:false},
-    {name:'Criação de Campi de Universidades no interior', checked:false},
-    {name:'Criação de Campi de Institutos Federais no interior', checked:false},
-    {name:'Fortalecimento do FIES', checked:false},
-    {name:'Cotas nas Universidades e IFs', checked:false},
-    {name:'Universidade Aberta do Brasil', checked:false}
-  ]
+    { name: "Criação do ProUni", checked: false },
+    { name: "Criação do ENEM e SiSu", checked: false },
+    { name: "Aumento de vagas nas Universidades Federais", checked: false },
+    { name: "Novas Universidades Federais", checked: false },
+    { name: "Criação de Campi de Universidades no interior", checked: false },
+    {
+      name: "Criação de Campi de Institutos Federais no interior",
+      checked: false
+    },
+    { name: "Fortalecimento do FIES", checked: false },
+    { name: "Cotas nas Universidades e IFs", checked: false },
+    { name: "Universidade Aberta do Brasil", checked: false }
+  ];
 
   againstTo = [
-    {name: 'Escola sem partido', checked: false},
-    {name: 'Ensino à distância desde o ensino básico', checked: false},
-    {name: 'Proibição da discussão sobre sexualidade nas escolas', checked: false},
-    {name: 'Privatização das Universidades Federais', checked: false},
-  ]
-  
+    { name: "Escola sem partido", checked: false },
+    { name: "Ensino à distância desde o ensino básico", checked: false },
+    {
+      name: "Proibição da discussão sobre sexualidade nas escolas",
+      checked: false
+    },
+    { name: "Privatização das Universidades Federais", checked: false }
+  ];
+
   firstGraduated = false;
-  firstGraduatedValue = "Sou o primeiro da família a completar o ensino superior";
-  
+  firstGraduatedValue =
+    "Sou o primeiro da família a completar o ensino superior";
+
   @Input()
   post: Post;
 
@@ -42,24 +49,26 @@ export class PostDetailsComponent {
   @Input()
   deleteHandler: Function;
 
-  constructor (private postService: PostService) {}
+  constructor(
+    private postService: PostService,
+    public authService: AuthService
+  ) {}
 
-  selectedOptions(list) { 
-    return list
-      .filter(opt => opt.checked)
-      .map(opt => opt.name)
+  selectedOptions(list) {
+    return list.filter(opt => opt.checked).map(opt => opt.name);
   }
 
-  selectedEduProgramOptions() { 
+  selectedEduProgramOptions() {
     return this.selectedOptions(this.eduProgramOptions);
   }
 
-  selectedAgainstToOptions() { 
+  selectedAgainstToOptions() {
     return this.selectedOptions(this.eduProgramOptions);
   }
 
   createPost(post: Post) {
-    if (this.firstGraduated) post.whoIAmToFamily += this.firstGraduatedValue + ". ";
+    if (this.firstGraduated)
+      post.whoIAmToFamily += this.firstGraduatedValue + ". ";
 
     let mEduProgramOptions = "";
     this.eduProgramOptions.forEach(option => {
@@ -75,7 +84,7 @@ export class PostDetailsComponent {
         mAgainstTo += option.name + ", ";
       }
     });
-    post.againstTo = mAgainstTo.slice(0, -1); 
+    post.againstTo = mAgainstTo.slice(0, -1);
 
     this.postService.createPost(post).then((newPost: Post) => {
       this.createHandler(newPost);
